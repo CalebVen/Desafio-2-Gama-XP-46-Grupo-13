5cbf861a5a1cc5cef059e7f1f0590123cff05f23
Renan Oshima <115747154+reoshma@users.noreply.github.com>
2022-11-18 17:03:09 -0300

    finalização edit js

--- editar-evento.js
+++ editar-evento.js
@@ -1,5 +1,6 @@
 console.log("Hello, wolrd!");
 
+const inputEvent = document.getElementsByTagName("form");
 const inputName = document.querySelector("#nome");
 const inputBanner = document.querySelector("#banner");
 const inputAttractions = document.querySelector("#atracoes");
@@ -10,6 +11,40 @@ const inputTicket = document.querySelector("#lotacao");
 const btnEditarEvento = document.querySelector("button");
 
 console.log(btnEditarEvento);
+
+
+inputEvent.onload = async () => {
+    const formInputLoad = {
+        name: inputName.value,
+        poster: inputBanner.value,
+        attractions: [
+            inputAttractions.value
+        ],
+        description: inputDescription.value,
+        scheduled:inputDate.value,
+        number_tickets: Number(inputTicket.value)
+        }; 
+
+        console.log(formInputLoad)
+
+        let URLid = new URLSearchParams(location.search);
+        let id_load = URLid.get("id")
+    
+    const resposta = await fetch(
+        'https://xp41-soundgarden-api.herokuapp.com/events/' + id_load
+      );
+  
+      const input = await resposta.json();     
+
+      inputName.value = input.name;
+      inputBanner.value = input.poster;
+      inputAttractions.value = input.attractions;
+      inputDescription.value = input.description;
+      inputDate.value = input.scheduled;
+      inputTicket.value = input.number_tickets;
+    };
+
+inputEvent.onload()
 
 async function setEdits(event) {
     event.preventDefault();
